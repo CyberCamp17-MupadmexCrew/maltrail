315b462f6ebde98d96784d9c4820f46b3b1a4c2c
abelrguezr <abelrguezr@gmail.com>
2017-12-02 20:10:54 +0100

    Minor changes on logger module.

--- processes/macos_sensor.py
+++ processes/macos_sensor.py
@@ -2,14 +2,13 @@ import subprocess
 import os
 import sys
 import logging
-import threading 
+import threading
 import logging.handlers
 
-_NOT_FOUND = '-1,'
+_NOT_FOUND = '-1'
 _LOG_PATH = "/tmp"                  # Absolute path for the log directory
 _LOG_FILESIZE = 100000              # in Bytes
 _LOG_COUNT = 1                      # number of logs to rotate
- 
 
 class MacOSSensor:
     def __init__(self):
@@ -22,46 +21,45 @@ class MacOSSensor:
                 proc = _process_file(_LOG_PATH+'/'+filename, prot, ip_dest, port_dest)
                 if (proc):
                     return proc
-        return _NOT_FOUND   
-    
+        return _NOT_FOUND
+
     def close(self):
         pass
-
 
  # Private functions
 
 def _init_thread():
     print '[i] Initializing tcpdump'
-    thread = threading.Thread(target=_print_tcpdump)        
-    thread.start()    
+    thread = threading.Thread(target=_print_tcpdump)
+    thread.start()
 
 def _init_logger():
     """
-     Sets up basic configuration using the given parameters for the logger module 
+     Sets up basic configuration using the given parameters for the logger module
      and defines log instances and handlers.
     """
 
-logging.basicConfig(stream=sys.stdout,level=logging.DEBUG)      
-logger = logging.getLogger('')  
-handler=logging.handlers.RotatingFileHandler(_LOG_PATH+'/tcpdump.log','a',_LOG_FILESIZE,_LOG_COUNT)    
+logging.basicConfig(stream=sys.stdout,level=logging.DEBUG)
+logger = logging.getLogger('')
+handler=logging.handlers.RotatingFileHandler(_LOG_PATH+'/tcpdump.log','a',_LOG_FILESIZE,_LOG_COUNT)
 logger.addHandler(handler)
 logger.setLevel(logging.DEBUG)
-    
+
 
 def _get_timestamp(line):
     """
      Extracts the timestamp of a log line
     """
     return line[:15]
-    
+
 def _get_proc(line):
     """
-     Extracts the process name and id for the given log line or _NOT_FOUND 
+     Extracts the process name and id for the given log line or _NOT_FOUND
     """
-    
+
     proc=line.partition('(')[-1].rpartition(')')[0]
     proc_name,_,proc_pid=proc.partition(' ')[-1].rpartition(':')
- 
+
     if((proc == None) or (')' in proc)):
         return _NOT_FOUND
     else:
@@ -69,11 +67,11 @@ def _get_proc(line):
             return _NOT_FOUND
         else:
             return proc_pid+','+proc_name
-    
-def _get_ips(line,mode=1): 
+
+def _get_ips(line,mode=1):
     """
      Extracts the ips for the given log line, the mode parameter is used to select the desired
-     IP returned. 
+     IP returned.
     """
 
     ips=line.partition('IP ')[-1].rpartition(': ')[0]
@@ -90,7 +88,7 @@ def _get_ips(line,mode=1):
 
 def _print_tcpdump():
     """
-     Writes the stdout of tcpdump to a file using the logger module 
+     Writes the stdout of tcpdump to a file using the logger module
     """
     c = subprocess.Popen((("tcpdump -nl -k NP").split()), stdout=subprocess.PIPE)
     for row in iter(c.stdout.readline, b''):
@@ -103,10 +101,9 @@ def _process_file(file, prot, ip_dest, port_dest):
 
     fread = open(file,'r')
     for line in reversed(fread.readlines()):
-        if(prot == 'icmp'):              
+        if(prot == 'icmp'):
             if(("ICMP" in line) and (ip_dest == _get_ips(line,2))):
                 return _get_proc(line)
         else:
             if ((ip_dest,port_dest) == _get_ips(line)):
                 return _get_proc(line)
-    
