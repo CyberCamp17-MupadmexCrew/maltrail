67ef514cefa9a8cfaa29b3dc0ac4ae1a3d5c00ce
Valentín Blanco <valentin_gomez1@hotmail.com>
2017-12-02 11:15:53 +0100

    Improved the process to ask an endpoint about the PID in the server

--- core/log.py
+++ core/log.py
@@ -30,6 +30,16 @@ from core.settings import TRAILS_FILE
 from core.settings import VERSION
 from core.ignore import ignore_event
 
+# Begin proc_sensor related consts.
+
+# Maximum buffer size for the response to use when calling proc_sensor server.
+_PROC_SENSOR_MAX_BUFFER = 1024
+
+# Maximum buffer size for the response to use when calling proc_sensor server.
+_PROC_SENSOR_REQ_PID_NAME = "get_pid"
+
+# End proc_sensor related consts.
+
 _condensed_events = {}
 _condensing_lock = threading.Lock()
 _thread_data = threading.local()
@@ -187,11 +197,62 @@ def log_error(msg):
             traceback.print_exc()
 
 def get_connection_data(event):
-    timestampSplit = event.split("\"", 3)
-    timestamp = timestampSplit[1]
-    ip_src, port_src, ip_dest, port_dest, prot_dest = timestampSplit[2].split(" ", 7)[2:7]
+    '''
+    Returns the relevant info about the connection of an event string.
+    Event data comes in the form:
+    '"2017-12-02 10:33:46.666285" ubuntu-laptop 10.100.1.29 61823 10.100.0.1 53 UDP DNS (raw).githubusercontent.com "malware distribution" malc0de.com'
+    :param event:
+    :return:
+    '''
+
+    # Split by " to get the timestamp part of the event.
+    timestamp_split = event.split("\"", 3)
+    timestamp = timestamp_split[1]
+
+    # The rest of relevant data can be retrieved splitting by space char.
+    ip_src, port_src, ip_dest, port_dest, prot_dest = timestamp_split[2].split(" ", 7)[2:7]
 
     return (timestamp, ip_src, port_src, ip_dest, port_dest, prot_dest)
+
+def get_connection_pid(event):
+    '''
+    Sends a request to the proc_sensor running in endpoints, asking for the PID that sent the packet
+    tracked in event, and returns the response.
+    Request example: 'get_pid,udp,192.168.0.200,8080,2017-07-03 23:29:32.689208'
+    Response example: '8392,wget www.google.com'
+    :param event:
+    :return:
+    '''
+
+    try:
+        # Retrieve the connection data from the event.
+        timestamp, ip_src, port_src, ip_dest, port_dest, prot_dest = get_connection_data(event)
+        # Change '-' char for a 0 in case we don't know the exact port (ICMP...)
+        port_src = port_src if port_src != "-" else "0"
+        port_dest = port_dest if port_dest != "-" else "0"
+        data_to_send = "%s,%s,%s,%s,%s" % (_PROC_SENSOR_REQ_PID_NAME, prot_dest, ip_dest, port_dest, timestamp)
+
+        if config.SHOW_DEBUG:
+            print("Requesting the endpoint for a PID: ", data_to_send)
+
+        # Tries to open a stream socket to the endpoint's proc_sensor and send the data.
+        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        s.connect((ip_src, config.PROC_SENSOR_PORT))
+        s.send(data_to_send)
+
+        # Retrieves the response from the socket.
+        data_received = s.recv(_PROC_SENSOR_MAX_BUFFER)
+        s.close()
+
+        if config.SHOW_DEBUG:
+            print("Response from the endpoint about the request for a PID: ", data_received)
+
+        # Return the info in a nice tuple.
+        pid_src, process_name_src = data_received.split(",", 2)
+        return pid_src, process_name_src
+    except:
+        if config.SHOW_DEBUG:
+            traceback.print_exc()
 
 def start_logd(address=None, port=None, join=False):
     class ThreadingUDPServer(SocketServer.ThreadingMixIn, SocketServer.UDPServer):
@@ -203,20 +264,18 @@ def start_logd(address=None, port=None, join=False):
                 data, _ = self.request
                 sec, event = data.split(" ", 1)
 
+                # Ask the endpoint running the proc_sensor for the PID that sent the packet tracked in event.
+                pid_info = get_connection_pid(event)
+
+                # If we got the PID info, add this to the event.
+                if pid_info:
+                    pid_src, process_name_src = pid_info
+                    event = "%s %s \"%s\"" % (event, pid_src, process_name_src)
+
                 handle = get_event_log_handle(int(sec), reuse=False)
                 os.write(handle, event)
                 os.close(handle)
 
-                timestamp, ip_src, port_src, ip_dest, port_dest, prot_dest = get_connection_data(event)
-                s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-                s.connect(("192.168.43.130", 2017))
-                toSend = "%s,%s,%s,%s,%s" % ("get_pid", prot_dest, ip_dest, port_dest, timestamp)
-                s.send(toSend)
-                #pid_src, process_name_src = s.recv(1024).split(",", 2)
-                received = s.recv(1024)
-                s.close()
-
-                print("Received: ", received)
             except:
                 if config.SHOW_DEBUG:
                     traceback.print_exc()
